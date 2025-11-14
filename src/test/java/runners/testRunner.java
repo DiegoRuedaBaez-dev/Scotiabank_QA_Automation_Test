7d7f8f40f483a4c990269d7fc8b5051ec1c641f1
Diego Rueda "Dante <diegofruedabaez@gmail.com>
2025-11-13 23:09:42 -0500

    Initialized testRunner.java for test execution

---
+++ src/test/java/runners/testRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = {"steps","utils"},
+        plugin = {"pretty",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        monochrome = true
+)
+
+public class testRunner{}
